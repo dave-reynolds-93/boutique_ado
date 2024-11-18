de3c0f9b6479451336e274079f0210836da64daf
David Reynolds <dr14598@my.bristol.ac.uk>
2024-11-18 14:42:16 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51QMVDMDywWjYZSgWhP1djobcVRd5fzwhj0b3sod3nKW7vISZDKGwKyWgYM63OEuIl8Tw4kd25HKiNrAkdh0Aj8de00RPtXCqRH',
+        'client_secret': 'test client secret',
     }
     
     return render(request, template, context)
